e05d3eaadd0c7158b63b1012dfa5ee4421e74e2a
Egoista <Egoista@github.com>
2020-03-30 18:02:58 +0800

    jiedui9

--- Game_life/src/jiedui/show.java
+++ Game_life/src/jiedui/show.java
@@ -5,17 +5,16 @@ import javax.swing.JPanel;
 public class show {
 	static boolean [][] state_one;
     static JPanel[][] jPanel;
-    //初始化类
+  
     public show(boolean [][] state,JPanel[][] jpanel_one)
     {
         state_one = state;
         jPanel = jpanel_one;
     }
-    //设置颜色
+    //配置细胞初始状态
     public static void paint()
     {
             
-        //循环判断状态设置颜色
         for(int i=1;i<state_one.length;i++)
         {
             for(int j=1;j<state_one[i].length-1;j++)
